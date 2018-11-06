bf7759095bfe955c12eb39fbfdd35dcc48036987
north1013 <fandersson50@gmail.com>
2018-11-06 10:54:45 +0100

    can successfully parse now

--- Decode-A-Web-Page.py
+++ Decode-A-Web-Page.py
@@ -6,15 +6,19 @@ from bs4 import BeautifulSoup
 def request(url):
     """Download the html of website"""
     r = requests.get(url)
-    echo (r.text)
+    return r.text
+    #print(r.text)
 
 def parsing(r_html):
     """Parse the html to get articles"""
-    soup = BeautifulSoup(r_html)
+    soup = BeautifulSoup(r_html, features='lxml')
+    title = soup.find('title')
+    print(title)
 
 
 def main(url):
+    #request(url)
     parsing(request(url))
 
-url = input("Input URL")
-main(url)
+URL = input("Input URL:\n")
+main(URL)
